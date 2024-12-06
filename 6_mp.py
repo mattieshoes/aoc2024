21201fdc5262c47bb043c5123bd2852fb9b8320d
MattieShoes <yourmom@mattie.shoes>
2024-12-06 13:55:55 -0700

    complex number version for 6.py, and a multiprocessing version

--- 6_mp.py
+++ 6_mp.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import concurrent.futures
+
 # Turns direction to the right, just for clarity
 # seems backwards because our Y axis is flipped -- "down" is increasing values
 def turn_right(direction):
@@ -7,9 +9,9 @@ def turn_right(direction):
 
 # Moves our guard forward as far forward as possible before hitting
 # an obstacle or moving off-grid.  returns resulting position
-def iterate(grid, pos, facing): 
+def iterate(grid, pos, facing, spot): 
     target = pos + facing
-    while target in grid and grid[target] != '#':
+    while target in grid and grid[target] != '#' and target != spot:
         target += facing
     if target in grid: # We hit an obstacle
         return target - facing
@@ -27,6 +29,7 @@ with open("inputs/6") as f:
 # if a key is in our dictionary.
 #
 # While we're at it, identify the starting position and direction
+
 grid = dict()
 starting_pos = 0+0j
 starting_facing = 0
@@ -60,19 +63,22 @@ print(f"Part 1: {part1}")
 #
 # Loop detection is just checking whether we've been at this location facing 
 # the same direction.
-part2 = 0
-visited.remove(starting_pos)
-for spot in visited:
-    grid[spot] = '#'
+
+def check(spot):
     pos = starting_pos
     facing = starting_facing
     locs = set()
     while pos in grid:
         if (pos, facing) in locs: # loop
-            part2 += 1
-            break
+            return 1
         locs.add((pos, facing))
-        pos = iterate(grid, pos, facing)
+        pos = iterate(grid, pos, facing, spot)
         facing = turn_right(facing)
-    grid[spot]='.'
+    return 0
+
+part2 = 0
+visited.remove(starting_pos)
+with concurrent.futures.ProcessPoolExecutor(max_workers = 4) as executor:
+    results = executor.map(check, visited)
+part2 = sum(results)
 print(f"Part 2: {part2}")
